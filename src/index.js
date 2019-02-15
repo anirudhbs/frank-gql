1b4ba6d63eed1fcffe4f8d83bff2bbdfec38db30
anirudhbs <anirudhbs96@gmail.com>
2019-02-15 11:42:28 +0530

    Add links in schema and resolver, using dummy data

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 const { GraphQLServer } = require("graphql-yoga")
+const { links } = require("./dummy-apis")
 
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  url: String!
+  description: String!
 }
 `
+
 // String! means info can never be null
+
+// Every GraphQL schema has three special root types,
+// these are called Query, Mutation and Subscription
 
 const resolvers = {
   Query: {
-    info: () => `This is what Frank Ocean is telling you, Hello!`
+    info: () => "Frank Ocean says Hello!",
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    url: parent => parent.url,
+    description: parent => parent.description
   }
 }
 
