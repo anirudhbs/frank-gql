e1d730aba86a4e939e29a86f9f2fc5919f156c90
anirudhbs <anirudhbs96@gmail.com>
2019-02-15 13:59:04 +0530

    Clean up, remove dummy API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga")
 
-const { links } = require("./dummy-apis")
 const { prisma } = require("./generated/prisma-client")
 
 // String! means info can never be null
@@ -29,34 +28,12 @@ const resolvers = {
     info: () => "Frank Ocean says Hello!",
     feed: (root, args, context) => {
       return context.prisma.links()
-    },
-    link: (parent, args) => links.filter(link => link.id === args.id)[0]
+    }
   },
   Mutation: {
     post: (root, args, context) => {
       const { url, description } = args
       return context.prisma.createLink({ url, description })
-    },
-    delete: (parent, args) => {
-      const { id } = args
-      const indexOfLink = links.findIndex(link => link.id === id)
-      const link = links[indexOfLink]
-      links.splice(indexOfLink, 1)
-      return link // return type has to be Link, as specified in schema
-    },
-    update: (parent, args) => {
-      const { id, url = null, description = null } = args
-      const indexOfLink = links.findIndex(link => link.id === id)
-
-      if (url !== null) {
-        links[indexOfLink].url = url
-      }
-
-      if (description !== null) {
-        links[indexOfLink].description = description
-      }
-
-      return links[indexOfLink]
     }
   }
 }
